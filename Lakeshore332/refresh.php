c768bb1dba68cc41538e7d0a554be169b1611c51
IvanUred <ivan.jakovac2@gmail.com>
2020-09-10 09:50:04 +0200

    First commit, no known bugs

---
+++ Lakeshore332/refresh.php
@@ -0,0 +1,23 @@
+<?php
+date_default_timezone_set("Europe/Zagreb");
+
+// Get old and new data
+
+$old_data = file_get_contents('log.txt');
+$new_data = $_POST['data'];
+$data = $old_data . "\n" . $new_data;
+
+// Explode data, keep only last # minutes
+
+$time_to_keep = "-6 hours";
+
+$lines = explode("\n", $data);
+
+foreach ($lines as $line) {
+	$temp = explode("\t", $line);
+	if(strtotime($temp[0])>strtotime($time_to_keep)) {
+		$array[] = implode("\t", $temp);
+	};
+}
+file_put_contents('log.txt', implode("\n", $array));
+?>
---
+++ readme.md
@@ -0,0 +1,13 @@
+NMR Service pages
+
+Responsive .html webpage shows live information from the following instruments:
+
+- Lakeshore 336
+	- VTI and probe temperature
+	- setpoint
+	- heater range & power
+- Lakeshore 332
+	- VTI and probe temperature
+	- setpoint
+	- heater range & power
+- Oxford Instruments ILM (He & N2 Levels)
